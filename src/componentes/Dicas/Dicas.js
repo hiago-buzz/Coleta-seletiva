70a6337b7d916345298a2f848af1d6651481c4af
hiago santos <foto.hiago@gmail.com>
2019-10-30 17:53:58 -0300

    atualizar topicos

--- src/componentes/Dicas/Dicas.js
+++ src/componentes/Dicas/Dicas.js
@@ -11,13 +11,13 @@ const Dicas = () => {
                     <h1>POR QUE RECICLAR?</h1>
                 </div>
                 <div className="topic">
-                    <Topico titulo="IMPORTÂNCIA DA RECICLAGEM" texto="fndksfnskfndkfndksfnkcxndskfnvkdsndksvnkxvnkvnkvnknvkxcnvkcx" />
-                    <Topico titulo="COMO DESCARTAR CORRETAMENTE" texto="fndksfnskfndkfndksfnkcxndskfnvkdsndksvnkxvnkvnkvnknvkxcnvkcx" />
+                    <Topico titulo="IMPORTÂNCIA DA RECICLAGEM" texto="A coleta seletiva é o recolhimento de materiais recicláveis que foram previamente separados do lixo comum pela população. Ao ser colocada em prática, ela permite que, por exemplo, vidro, papel, plástico e metais sejam utilizados na confecção de novos produtos. O reaproveitamento reduz a quantidade de resíduos que é destinada ao aterro sanitário, aumentando sua vida útil e preserva o meio ambiente." />
+                    <Topico titulo="COMO DESCARTAR CORRETAMENTE" texto="A coleta seletiva é o recolhimento de materiais recicláveis que foram previamente separados do lixo comum pela população. Ao ser colocada em prática, ela permite que, por exemplo, vidro, papel, plástico e metais sejam utilizados na confecção de novos produtos. O reaproveitamento reduz a quantidade de resíduos que é destinada ao aterro sanitário, aumentando sua vida útil e preserva o meio ambiente." />
                 </div>
                 <div className="topic">
 
-                    <Topico titulo="BENEFICIOS" texto="fndksfnskfndkfndksfnkcxndskfnvkdsndksvnkxvnkvnkvnknvkxcnvkcx" />
-                    <Topico titulo="MELHORANDO SUA CIDADE" texto="fndksfnskfndkfndksfnkcxndskfnvkdsndksvnkxvnkvnkvnknvkxcnvkcx" />
+                    <Topico titulo="BENEFICIOS" texto="A coleta seletiva é o recolhimento de materiais recicláveis que foram previamente separados do lixo comum pela população. Ao ser colocada em prática, ela permite que, por exemplo, vidro, papel, plástico e metais sejam utilizados na confecção de novos produtos. O reaproveitamento reduz a quantidade de resíduos que é destinada ao aterro sanitário, aumentando sua vida útil e preserva o meio ambiente." />
+                    <Topico titulo="MELHORANDO SUA CIDADE" texto="A coleta seletiva é o recolhimento de materiais recicláveis que foram previamente separados do lixo comum pela população. Ao ser colocada em prática, ela permite que, por exemplo, vidro, papel, plástico e metais sejam utilizados na confecção de novos produtos. O reaproveitamento reduz a quantidade de resíduos que é destinada ao aterro sanitário, aumentando sua vida útil e preserva o meio ambiente." />
                 </div>
                 </article>
               
