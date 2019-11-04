723c628b69fe18b711fd7b241dbf6b6121067f5f
hiago santos <foto.hiago@gmail.com>
2019-11-04 17:40:16 -0300

    ajustes responsivos

--- src/componentes/Nav/Nav.js
+++ src/componentes/Nav/Nav.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Nav.scss'
 import {Link, animateScroll as scroll} from "react-scroll"
 import Botao from '../Botao/Botao';
 
 const Nav = () => {
 
-    const scrollToTop = () =>{
-        scroll.scrollToTop();
+    const [menuAtivo, setMenuAtivo] = useState(false);
+
+    const AbrirMenu = () => {
+        setMenuAtivo(!menuAtivo);
     };
+
+    const FecharMenu = () =>{
+        setMenuAtivo(false)
+    }
+   
 
     return (
         <div className="Nav">
@@ -25,11 +32,19 @@ const Nav = () => {
                     <Link to="Acesso" spy={true} smooth={true} offset={-70} duration={500}> <Botao classe="btn-cadastro">Cadastrar</Botao></Link>
                     <Link to="Acesso" spy={true} smooth={true} offset={-70} duration={500}><Botao>Entrar</Botao></Link>
                 </div>
-                <div>
-                    <span></span>
-                    <span></span>
-                    <span></span>
+                <div onClick={AbrirMenu} id="menu-hamburguer">
+                    <span className={`${menuAtivo ? "virarX1" : ""}`}></span>
+                    <span className={`${menuAtivo ? "sumirIgualTiro" : ""}`}></span>
+                    <span className={`${menuAtivo ? "virarX2" : ""}`}></span>
                 </div>
+            
+            <section className={`menu ${menuAtivo ? "ativo" : ""}`}>
+              
+                <Link onClick={FecharMenu} to="Nav" spy={true} smooth={true} offset={-70} duration={500}>Home</Link>
+                <Link onClick={FecharMenu} to="Dicas" spy={true} smooth={true} offset={-70} duration={500}>Dicas</Link>
+                <Link onClick={FecharMenu} to="Noticias" spy={true} smooth={true} offset={-70} duration={500}>Noticias</Link>
+                <Link onClick={FecharMenu} to="Coleta" spy={true} smooth={true} offset={-70} duration={500}>Pontos de Descarte</Link>
+            </section>
             </nav>
         </div>
     )
