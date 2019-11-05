5940efa4c24ad45d45cce51d061dcedc0cd706b5
hiago santos <foto.hiago@gmail.com>
2019-11-04 23:01:09 -0300

    inserir logo

--- src/componentes/Nav/Nav.js
+++ src/componentes/Nav/Nav.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import './Nav.scss'
 import {Link} from "react-scroll"
 import Botao from '../Botao/Botao';
+import Logo from '../img/logo.png'
 
 const Nav = () => {
 
@@ -20,14 +21,13 @@ const Nav = () => {
         <div className="Nav">
             <nav>
                 <div>
-                    <h2>COLETA SELETIVA</h2>
+                <Link to="Nav" spy={true} smooth={true} offset={-70} duration={500}><img src={Logo} alt="logo"/></Link>
                 </div>
                 <div className="Ancoras">
-                    <Link to="Nav" spy={true} smooth={true} offset={-70} duration={500}>Home</Link>
+                   
                     <Link to="Dicas" spy={true} smooth={true} offset={-70} duration={500}>Dicas</Link>
                     <Link to="Coleta" spy={true} smooth={true} offset={-70} duration={500}>Pontos de Entrega</Link>
                     <Link to="Noticias" spy={true} smooth={true} offset={-70} duration={500}>Noticias</Link>
-                    
                     <Link to="Contato" spy={true} smooth={true} offset={-70} duration={500}>Contatos</Link>
                     <Link to="Acesso" spy={true} smooth={true} offset={-70} duration={500}> <Botao classe="btn-cadastro">Cadastrar</Botao></Link>
                     <Link to="Acesso" spy={true} smooth={true} offset={-70} duration={500}><Botao>Entrar</Botao></Link>
@@ -40,12 +40,13 @@ const Nav = () => {
             
             <section className={`menu ${menuAtivo ? "ativo" : ""}`}>
               
-                <Link onClick={FecharMenu} to="Nav" spy={true} smooth={true} offset={-70} duration={500}>Home</Link>
                 <Link onClick={FecharMenu} to="Dicas" spy={true} smooth={true} offset={-70} duration={500}>Dicas</Link>
                 <Link onClick={FecharMenu} to="Coleta" spy={true} smooth={true} offset={-70} duration={500}>Pontos de Entrega</Link>
                 <Link onClick={FecharMenu} to="Noticias" spy={true} smooth={true} offset={-70} duration={500}>Noticias</Link>
                 <Link onClick={FecharMenu} to="Acesso" spy={true} smooth={true} offset={-70} duration={500}>Cadastrar</Link>
                 <Link onClick={FecharMenu} to="Acesso" spy={true} smooth={true} offset={-70} duration={500}>Entrar</Link>
+                <Link onClick={FecharMenu} to="Contato" spy={true} smooth={true} offset={-70} duration={500}>Contatos</Link>
+
             </section>
             </nav>
         </div>
