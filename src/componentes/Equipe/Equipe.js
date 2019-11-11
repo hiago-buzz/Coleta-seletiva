a34bdaa0ef35bc014722c35c81ea7164e128e1ba
hiago santos <foto.hiago@gmail.com>
2019-11-11 17:56:13 -0300

    perfil!

--- src/componentes/Equipe/Equipe.js
+++ src/componentes/Equipe/Equipe.js
@@ -11,6 +11,9 @@ const Equipe = () => {
     
     const [mostrar, setMostrar] = useState(false)
     const [mostrar2, setMostrar2] = useState(false)
+    const [mostrar3, setMostrar3] = useState(false)
+    const [mostrar4, setMostrar4] = useState(false)
+    const [mostrar5, setMostrar5] = useState(false)
 
     const trocarUm = ()=>{
         if(!mostrar){
@@ -32,11 +35,39 @@ const Equipe = () => {
         
     }
 
+
+    const trocarTres = ()=>{
+        if(!mostrar3){
+            setMostrar3(!mostrar3)
+        }
+        else if(mostrar3){
+            setMostrar3(!mostrar3)
+        }
+        
+    }
+
+
+    const trocarQuatro = ()=>{
+        if(!mostrar4){
+            setMostrar4(!mostrar4)
+        }
+        else if(mostrar4){
+            setMostrar4(!mostrar4)
+        }
+        
+    }
+    const trocarCinco = ()=>{
+        if(!mostrar5){
+            setMostrar5(!mostrar5)
+        }
+        else if(mostrar5){
+            setMostrar5(!mostrar5)
+        }
+        
+    }
     
     
-    // useEffect(()=>{
-    //     trocarEstado()
-    // }, [mostrar])
+    
 
     return (
         <div className="Equipe">
@@ -44,11 +75,11 @@ const Equipe = () => {
          <header>
              <h2>EQUIPE DE DESENVOLVEDORES</h2>
          </header>
-         <Perfil imagemPerfil={hiago} saturacao={!mostrar ? {filter: "grayscale(1)"} : {filter: "grayscale(0)"}} nomePerfil="HIAGO - DEV FULLSTACK" pop={!mostrar ? "+" :"-"}  click={trocarUm} />
+         <Perfil imagemPerfil={hiago} nomePerfil="HIAGO - DEV FULLSTACK" pop={!mostrar ? "+" :"-"}  click={trocarUm} />
          <Aberto style={!mostrar ? {display: "none"} : {display:"flex"}} nomeCompleto="Cícero Hiago Santos" descricaoPerfil="Desenvolvedor Fullstack com Foco em React.js e Django REST." linkLinkedin="https://www.linkedin.com/in/hiagosantos" linkGithub="https://github.com/hiago-buzz" linkWhatsapp="https://api.whatsapp.com/send?l=pt&phone=5511944632951" emailPerfil="hiago.dev@gmail.com"/>
          
          <Perfil imagemPerfil={imagemuser} nomePerfil="LUCAS - DEV FULLSTACK" pop={!mostrar2 ? "+" :"-"}  click={trocarDois} />
-         <Aberto style={!mostrar2 ? {display: "none"} : {display:"flex"}} nomeCompleto="Lucas Barletta" descricaoPerfil="Desenvolvedor Fullstack com Foco em React.js e Django REST." emailPerfil="hiago.dev@gmail.com"/>
+         <Aberto style={!mostrar2 ? {display: "none"} : {display:"flex"}} nomeCompleto="Lucas Barletta" descricaoPerfil="Desenvolvedor Fullstack com Foco em C# e Django REST." emailPerfil="lubarlettamunhoz@gmail.com"/>
 
          <Perfil imagemPerfil={imagemuser} nomePerfil="JESSICA - DEV FULLSTACK" pop={!mostrar2 ? "+" :"-"}  click={trocarDois} />
          <Aberto style={!mostrar2 ? {display: "none"} : {display:"flex"}} nomeCompleto="Jessica Santos" descricaoPerfil="Desenvolvedor Fullstack com Foco em React.js e Django REST." emailPerfil="hiago.dev@gmail.com"/>
